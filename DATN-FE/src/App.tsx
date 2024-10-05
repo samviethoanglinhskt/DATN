b5f5f27021fc9bf5a4ea3084d423692aca52a2ab
hoangtran-dev <hoangviettran.dev@gmail.com>
2024-10-05 20:43:24 +0700

    update product

--- DATN-FE/src/App.tsx
+++ DATN-FE/src/App.tsx
@@ -1,26 +1,24 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './component/Header/Header'
-import Slider from './component/Home/Slider'
-import Cart from './component/Home/Cart'
-import Banner from './component/Home/Banner'
-import Product from './component/Home/Product'
-import Blog from './component/Home/Blog'
+// import Slider from './component/Home/Slider'
+// import Cart from './component/Home/Cart'
+// import Banner from './component/Home/Banner'
+// import Product from './component/Home/Product'
+// import Blog from './component/Home/Blog'
 import Footer from './component/Footer/Footer'
+import CartProduct from './component/ProducPages/CartProdduct'
+import ProductPages from './component/ProducPages/Product'
 
 function App() {
 
 
   return (
     <>
-    <Header />
-    <Slider />
-    <Cart />
-    <Slider />
-    <Banner />
-    <Product />
-    <Blog />
-    <Footer />
+      {/* <Header /> */}
+      <CartProduct />
+      <ProductPages />
+      {/* <Footer /> */}
     </>
   )
 }
