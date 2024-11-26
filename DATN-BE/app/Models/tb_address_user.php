9f9d76c0ecd611e1e79ed3e955a526423224105a
huyphuc204 <phucvh204@gmail.com>
2024-11-27 00:40:19 +0700

    Thêm model address và migration

---
+++ DATN-BE/app/Models/tb_address_user.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class tb_address_user extends Model
+{
+    use HasFactory;
+
+    protected $table = 'tb_address_users';
+    protected $fillable = [
+        'user_id',
+        'address',
+        'address_detail'
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
